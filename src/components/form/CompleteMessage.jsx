474c03ed8a92f22b5d8127392ec4e6f030d152d5
cc-yujin <mooo7570@gmail.com>
2024-09-09 16:28:37 +0900

    [ADD] JoinDonePage 퍼블리싱

--- src/components/form/CompleteMessage.jsx
+++ src/components/form/CompleteMessage.jsx
@@ -1,12 +1,31 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
+import { ReactComponent as CompleteIcon } from "@svgs/complete.svg";
 
-export default function CompleteMessage({ icon }) {
+export default function CompleteMessage({ icon, text, highlight }) {
   return (
     <div css={message_wrap}>
-      <div css={icon}>{icon}</div>
+      <CompleteIcon css={icon} />
+      <p>{text}</p>
+      {highlight && <p css={highlight_style}>{highlight}</p>}
     </div>
   );
 }
 
-const message_wrap = css``;
+const message_wrap = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 12px;
+  padding: 54px 0 44px;
+
+  p {
+    font-size: 24px;
+    font-weight: 700;
+    line-height: 28.8px;
+    color: #3c3c3c;
+  }
+`;
+
+const highlight_style = css``;
--- src/pages/JoinDonePage/JoinDonePage.jsx
+++ src/pages/JoinDonePage/JoinDonePage.jsx
@@ -1,3 +1,21 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import FormHeader from "@components/form/FormHeader";
+import CompleteMessage from "@components/form/CompleteMessage";
+import Button from "@components/ui/Button";
+
 export default function JoinDonePage() {
-  return <div>JoinDonePage</div>;
+  return (
+    <div css={page_wrap}>
+      <FormHeader label="회원가입" />
+      <CompleteMessage text="회원가입이 완료되었습니다." />
+      <Button label={"로그인"} />
+    </div>
+  );
 }
+
+const page_wrap = css`
+  padding: 250px 0 443px;
+  width: 500px;
+  margin: 0 auto;
+`;
