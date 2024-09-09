6e09d00d7f0abfd21734e77d914da45f866dbab0
cc-yujin <mooo7570@gmail.com>
2024-09-09 17:51:59 +0900

    [ADD] 그라데이션 배경 적용

--- src/pages/JoinDonePage/JoinDonePage.jsx
+++ src/pages/JoinDonePage/JoinDonePage.jsx
@@ -3,19 +3,22 @@ import { css } from "@emotion/react";
 import FormHeader from "@components/form/FormHeader";
 import CompleteMessage from "@components/form/CompleteMessage";
 import Button from "@components/ui/Button";
+import GradientBack from "@components/ui/GradientBack";
 
 export default function JoinDonePage() {
   return (
-    <div css={page_wrap}>
-      <FormHeader label="회원가입" />
-      <CompleteMessage text="회원가입이 완료되었습니다." />
-      <Button label={"로그인"} />
-    </div>
+    <GradientBack>
+      <div css={page_wrap}>
+        <FormHeader label="회원가입" />
+        <CompleteMessage text="회원가입이 완료되었습니다." />
+        <Button label={"로그인"} />
+      </div>
+    </GradientBack>
   );
 }
 
 const page_wrap = css`
-  padding: 250px 0 443px;
+  padding: 85px 0 404px;
   width: 500px;
   margin: 0 auto;
 `;
--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -4,21 +4,23 @@ import FormHeader from "@components/form/FormHeader";
 import FormLink from "@components/form/FormLink";
 import Button from "@components/ui/Button";
 import FormInput from "@components/form/FormInput";
-// import GradientBack from "../../components/ui/GradientBack";
+import GradientBack from "../../components/ui/GradientBack";
 
 export default function LoginPage() {
   return (
-    <div css={login_wrap}>
-      <FormHeader label="로그인" />
+    <GradientBack>
+      <div css={login_wrap}>
+        <FormHeader label="로그인" />
 
-      <div css={form_wrapper}>
-        <FormInput label="이메일" placeholder="이메일주소를 입력하세요." />
-        <FormInput label="비밀번호" placeholder="비밀번호를 입력하세요." />
+        <div css={form_wrapper}>
+          <FormInput label="이메일" placeholder="이메일주소를 입력하세요." />
+          <FormInput label="비밀번호" placeholder="비밀번호를 입력하세요." />
+        </div>
+
+        <Button label={"로그인"} />
+        <FormLink hasBorder />
       </div>
-
-      <Button label={"로그인"} />
-      <FormLink hasBorder />
-    </div>
+    </GradientBack>
   );
 }
 
@@ -29,7 +31,7 @@ const login_wrap = css`
 
   width: 500px;
   margin: 0 auto;
-  padding: 250px 0 292px;
+  padding: 85px 0 292px;
 `;
 
 const form_wrapper = css`
